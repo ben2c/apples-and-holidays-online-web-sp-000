28d269ea0ab036c1c40e7e616ef9d9e56ed8dba3
Benjamin Cheng <bencheng200@gmail.com>
2020-11-14 06:09:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season.upcase}: "
+    puts "#{season.upcase}:"
   end
 end
 
